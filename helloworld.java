0838c7dfdc01170560a31dcc146282ebcb768307
nani454554 <119169444+nani454554@users.noreply.github.com>
2023-06-24 14:47:47 +0100

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello batch30 team with joy , rushwanth  and team");
-      System.out.println("Hello nikhil you have completed build and compile process ")
+      System.out.println("Hello nikhil you have completed build and compile process ");
           }
 } 
